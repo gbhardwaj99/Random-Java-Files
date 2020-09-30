6dcd95a88127d44e50820a6a653005cca8005da0
Achint Srivastava <32853258+BabaYaga007@users.noreply.github.com>
2020-10-01 05:22:22 +0530

    Added a program to print a pattern

--- aga1n.java
+++ aga1n.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
-public static void main()
-{
-	System.out.println("Again\n");
+// public static void main()
+// {
+// 	System.out.println("Again\n");
+// }
+
+// A program to print a pattern
+public class PatternOne {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter string");
+		
+		String s=sc.next();
+		int len = s.length();
+		for(int x=0;x<len;x++)
+		{
+		    for(int k=len-x-1;k<len;k++) {
+		    System.out.print(s.charAt(k));   
+		    }
+		System.out.println();
+		}
+	}
 }
