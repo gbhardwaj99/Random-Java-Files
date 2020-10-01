9903e49d840361664ea6f60029dd0167576afc1f
Achint Srivastava <32853258+BabaYaga007@users.noreply.github.com>
2020-10-01 05:31:13 +0530

    Just some Keanu Reeves changes

--- w0rld.java
+++ w0rld.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
-public static void main()
+public class Main
 {
-	System.out.println("World\n");
+	public static void main(String[] args) 
+	{
+	System.out.println("Hello World\n");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("What do you want to say?");
+	String s=sc.next();
+	System.out.println("The person says - You are Breathtaking & "+s);
+
+	}
 }
